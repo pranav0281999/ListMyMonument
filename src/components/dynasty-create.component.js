39879b4c45b369721e6f7f2063769ca7b46c9c5f
Pranav Bhardwaj <pranav0281999@gmail.com>
2019-10-15 22:00:02 +0530

    Updates dynasty-create component to add form

--- src/components/dynasty-create.component.js
+++ src/components/dynasty-create.component.js
@@ -1,9 +1,81 @@
-import React from 'react';
+import React, { Component } from 'react';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-export default class DynastyCreate extends React.Component {
+export default class DynastyCreate extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      date: new Date(),
+      users: ['test user 3', 'test user 4']
+    }
+  }
+
+  componentDidMount = () => {
+    this.setState({
+      name: "Dynasty Name",
+      user: ['test user', 'test user 2']
+    });
+  }
+
+  onChangeName = (e) => {
+    this.setState({
+      name: e.target.value
+    });
+  }
+
+  onChangeDate = (date) => {
+    this.setState({
+      date: date
+    });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    const dynasty = {
+      name: this.state.name,
+      date: this.state.date
+    }
+
+    console.log(dynasty);
+
+    window.location = "/";
+  }
+
   render() {
     return (
-      <p>DynastyCreate</p>
+      <div>
+        <h3>Create New Dynasty</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Name: </label>
+            <select ref="userInput" required className="form-control" value={this.state.name} onChange={this.onChangeName}>
+              {
+                this.state.users.map(
+                  (user) => {
+                    return <option
+                      key={user}
+                      value={user}>
+                      {user}
+                    </option>
+                  }
+                )
+              }
+            </select>
+          </div>
+          <div className="form-group">
+            <label>Date: </label>
+            <DatePicker selected={this.state.date} onChange={this.onChangeDate}></DatePicker>
+          </div>
+          <div className="form-group">
+            <input type="submit" value="Create Dynasty" className="btn btn-primary"></input>
+          </div>
+        </form>
+      </div>
     );
   }
 }
